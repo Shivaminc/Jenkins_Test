f0059f5ac7ca58b5011c7ec65e074c3cbee93488
Shivaminc. <54238581+Shivaminc@users.noreply.github.com>
2024-08-28 15:28:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
-// index.php
-
 $tasks = json_decode(file_get_contents('tasks.json'), true);
 ?>
 <h1>Todo List</h1>
 <ul>
-    <?php foreach ($tasks as $task): ?>
-        <li><?php echo htmlspecialchars($task['task']); ?></li>
+    <?php foreach ($tasks as $id => $task): ?>
+        <li>
+            <?php echo htmlspecialchars($task['task']); ?>
+            <a href="update.php?id=<?php echo $id; ?>">Update</a>
+            <a href="delete.php?id=<?php echo $id; ?>" onclick="return confirm('Are you sure?')">Delete</a>
+        </li>
     <?php endforeach; ?>
 </ul>
 <a href="add.php">Add New Task</a>
