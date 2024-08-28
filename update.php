259ad94676d3594461f10e7017e8e8ad8cc385d9
Shivaminc. <54238581+Shivaminc@users.noreply.github.com>
2024-08-28 15:12:32 +0530

    Create update.php

---
+++ update.php
@@ -0,0 +1,25 @@
+<?php
+// update.php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = $_POST['id'];
+    $newTask = $_POST['task'];
+
+    if (!empty($id) && !empty($newTask)) {
+        $tasks = json_decode(file_get_contents('tasks.json'), true);
+        $tasks[$id]['task'] = $newTask;
+        file_put_contents('tasks.json', json_encode($tasks, JSON_PRETTY_PRINT));
+        header('Location: index.php'); // Redirect to list of tasks
+        exit();
+    }
+}
+
+$taskId = $_GET['id'];
+$tasks = json_decode(file_get_contents('tasks.json'), true);
+$task = $tasks[$taskId];
+?>
+<form method="POST">
+    <input type="hidden" name="id" value="<?php echo htmlspecialchars($taskId); ?>">
+    <input type="text" name="task" value="<?php echo htmlspecialchars($task['task']); ?>">
+    <button type="submit">Update Task</button>
+</form>
